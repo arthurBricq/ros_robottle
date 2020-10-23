d3e901cae78f93dc5ae581e435564176556e0627
Arthur Bricq <arthur.bricq@epfl.ch>
2020-10-23 17:01:25 +0200

    adding open loop controller, and uart node as well (stil not working)

--- src/robottle/robottle/controller_ol.py
+++ src/robottle/robottle/controller_ol.py
@@ -1,23 +1,23 @@
 import rclpy
 from rclpy.node import Node
+import numpy as np 
 
 from interfaces.msg import Map
 from interfaces.msg import Position
+from std_msgs.msg import String
 
-from roboviz import MapVisualizer
+from robottle_utils import map_utils
 
 MAP_SIZE_PIXELS         = 500
 MAP_SIZE_METERS         = 10
 
-
-
-class SlamVizualizer(Node):
+class ControllerOpenLoop(Node):
     """
     This node visualize the map received from the SLAM node.
     This node is quite hard to use, because it slows down a lot the process. It should not be used in live with the Jetson 
     """
     def __init__(self):
-        super().__init__("slam_viz")
+        super().__init__("controller1")
 
         # Create subscription for the map 
         self.subscription1 = self.create_subscription(
@@ -34,19 +34,14 @@ class SlamVizualizer(Node):
             self.listener_callback_map,
             1000)
         self.subscription2  # prevent unused variable warning
-
-        # setup the vizualistion tool (it will open a window showing the map and the robot within the map)
-        self.viz = MapVisualizer(MAP_SIZE_PIXELS, MAP_SIZE_METERS, "SLAM")
+        
+        # Create a publication for uart commands
+        self.uart_publisher = self.create_publisher(String, 'uart_commands', 1000)
 
         # keep track of where is the robot within the class
         self.x = 0
         self.y = 0
         self.theta = 0 
-
-    def listener_callback_map(self, map_message):
-        self.get_logger().info("map received : {}".format(map_message.index))
-        map_data = bytearray(map_message.map_data)
-        # if not self.viz.display(self.x, self.y, self.theta, map_data): exit(0)
 
 
     def listener_callback_position(self, pos):
@@ -55,9 +50,34 @@ class SlamVizualizer(Node):
         self.theta = pos.theta
         self.get_logger().info("position received from SLAM: {}, {}, {}  ".format(self.x,self.y,self.theta))
 
+
+    def listener_callback_map(self, map_message):
+        self.get_logger().info("map received : {}".format(map_message.index))
+        map_data = bytearray(map_message.map_data)
+        occupancy_grid = map_utils.get_map(map_data)
+        
+        # avoid wrong cases
+        if self.x == 0 and self.y == 0: return
+
+        # detect if there is an obstacle in front of the robot using purely the LIDAR data
+        has_obstacle = map_utils.inspect_line(
+                occupancy_grid,
+                robot_position = (self.x, self.y, self.theta),
+                length = 2)
+        self.get_logger().info("has obstacle : {}".format(has_obstacle))
+        
+        # publish it to the uart node
+        msg = String()
+        if has_obstacle: # left or right
+            msg.data = "a"
+        else:
+            msg.data = "w"
+        self.uart_publisher.publish(msg)
+
+
 def main(args=None):
     rclpy.init(args=args)
-    node = SlamVizualizer()
+    node = ControllerOpenLoop()
     rclpy.spin(node)
     rclpy.shutdown()
 
---
+++ src/robottle/robottle/uart_messenger.py
@@ -0,0 +1,55 @@
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import String
+
+import time
+import serial
+
+MAP_SIZE_PIXELS         = 500
+MAP_SIZE_METERS         = 10
+
+class UARTMessenger(Node):
+    """
+    This node receives String message and sends them to the UART channel for the Arduino
+    """
+
+    def __init__(self):
+        super().__init__("uart_messenger")
+
+        # Create subscription for the uart commands to send 
+        self.subscription1 = self.create_subscription(
+            String,
+            'uart_commands',
+            self.listener_callback,
+            1000)
+        self.subscription1  # prevent unused variable warning
+
+        # setup the uart port and wait a second for it
+        self.serial_port = serial.Serial(
+            port="/dev/ttyTHS1",
+            baudrate=115200)
+            
+        time.sleep(1)
+        
+        self.i = 0
+        self.serial_port.write("hello arduino".encode())
+
+    def listener_callback(self, msg):
+        self.i += 1
+        if self.i % 5 == 0:
+            self.get_logger().info(msg.data)
+            self.serial_port.write(msg.data.encode())
+
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = UARTMessenger()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
+
