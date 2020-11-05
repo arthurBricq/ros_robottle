f6e156d7afdb87b6a1632f2e0c5cee9e18f49a08
Arthur Bricq <arthur.bricq@epfl.ch>
2020-11-05 18:37:42 +0100

    Adding odometry 1.0 and making slam better

--- src/robottle/robottle/lidar_publisher.py
+++ src/robottle/robottle/lidar_publisher.py
@@ -1,17 +1,3 @@
-# Copyright 2016 Open Source Robotics Foundation, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import rclpy
 from rclpy.node import Node
 
@@ -19,15 +5,13 @@ from std_msgs.msg import String
 from interfaces.msg import LidarData 
 
 from breezyslam.sensors import RPLidarA1 as LaserModel
-
 from breezyslam.algorithms import RMHC_SLAM
-
 from rplidar import RPLidar as Lidar
+
 
 MAP_SIZE_PIXELS         = 500
 MAP_SIZE_METERS         = 10
 LIDAR_DEVICE = '/dev/ttyUSB0'
-
 
 class LidarPublisher(Node):
 
@@ -35,9 +19,6 @@ class LidarPublisher(Node):
         super().__init__('minimal_publisher')
         # create publisher
         self.publisher_ = self.create_publisher(LidarData, 'lidar_data', 10)
-        
-        # create timer that will triger reading the data
-        self.i = 0
 
         # setup the lidar object
         self.lidar = Lidar(LIDAR_DEVICE)
--- src/robottle/robottle/slam.py
+++ src/robottle/robottle/slam.py
@@ -9,11 +9,15 @@ from interfaces.msg import Nums
 from interfaces.msg import LidarData 
 from interfaces.msg import Position
 from interfaces.msg import Map
+from interfaces.msg import MotorsSpeed
 
 # imports things for slam
 from breezyslam.algorithms import RMHC_SLAM
 from breezyslam.sensors import RPLidarA1 as LaserModel
+import numpy as np
 
+# import lidar library to analyse angles
+from robottle_utils import lidar_utils
 
 # Constants for run time
 MIN_SAMPLES = 100 
@@ -23,20 +27,21 @@ MAP_SIZE_METERS         = 10
 class Slam(Node):
     """
     SLAM NODE
-    This node is in charge of receiving data from the different sources and creating a map of the environment
+    This node is in charge of receiving data from the LIDAR and creating a map of the environment
     while locating the robot in the map.
 
     INPUT
-    - Bottle detected by the Neuron
+    - Bottle detected by the Neuron (to be removed soon)
     - Lidar detections
 
     OUTPUT
     - Robot position: /robot_pos
     - Map as a byte array: /world_map
+    (this map can be interpred using the map_utils.py package !)
     """
+
     def __init__(self):
         super().__init__('slam_node')
-
 
         # Create first subscripton (detectnet results)
         self.subscription1 = self.create_subscription(
@@ -54,6 +59,14 @@ class Slam(Node):
             1000)
         self.subscription2  # prevent unused variable warning
 
+        # Create third subscription (Motor speeds to compute robot pose change)
+        self.subscription3 = self.create_subscription(
+            MotorsSpeed,
+            'motors_speed',
+            self.listener_callback_motorsspeed,
+            1000)
+        self.subscription3  # prevent unused variable warning
+
         # Create publisher for the position of the robot, and for the map 
         self.publisher_position = self.create_publisher(Position, 'robot_pos', 10)
         self.publisher_map = self.create_publisher(Map, 'world_map', 10)
@@ -64,30 +77,58 @@ class Slam(Node):
         self.mapbytes = bytearray(MAP_SIZE_PIXELS * MAP_SIZE_PIXELS)
         self.previous_distances = None
         self.previous_angles    = None
-        self.map_index = 0 
+        self.map_index = 0
+        self.robot_pose_change = np.array([0.0,0.0,0.0])
 
- 
 
     def listener_callback_detectnet(self, msg):
         self.get_logger().info('I heard something from vision ! ')
 
+    def listener_callback_motorsspeed(self, msg):
+        """
+        Must compute the robot pose change since the last time the data was collected from the motor
+        The input are the motors speed and the time change.
 
+        Documentation of the SLAM library says :
+            "pose_change is a tuple (dxy_mm, dtheta_degrees, dt_seconds) computed from odometry"
+        """
+        dx_left = msg.RADIUS * msg.left * (0.1047) * msg.time_delta
+        dx_right = msg.RADIUS * msg.right * (0.1047) * msg.time_delta
+        delta_r = 1/2 * (dx_left + dx_right) # [mm]
+        delta_d =  dx_right - dx_left
+        delta_theta = np.arctan(delta_d / msg.LENGTH) * 57.2958 # [deg]
+        self.robot_pose_change += [delta_r, delta_theta, msg.time_delta]
+        self.get_logger().info("robot pose change: {}".format(self.robot_pose_change))
 
     def listener_callback_lidar(self, msg):
-        self.get_logger().info('I heard something from lidar ! ')
-        
+        # UNCOMMENT this code to find indexes i1 and i2
+
+        (i1, i2) = lidar_utils.get_valid_lidar_range(distances = msg.distances, 
+                angles = msg.angles,
+                n_points = 10)
+        self.get_logger().info('indexes : {} : {} with angles {} - {}'
+                .format(i1,i2,msg.angles[i1], msg.angles[i2]))
+
+        # extract angles of interest
+        angles = list(msg.angles[i1:i2])
+        distances = list(msg.distances[i1:i2])
+
+        # compute robot pose now
+        # todo
+
         # Update SLAM with current Lidar scan and scan angles if adequate
-        if len(msg.distances) > MIN_SAMPLES:
-            self.slam.update(list(msg.distances), scan_angles_degrees=list(msg.angles))
-            self.previous_distances = list(msg.distances).copy()
-            self.previous_angles    = list(msg.angles).copy()
+        if len(distances) > MIN_SAMPLES:
+            self.slam.update(distances, scan_angles_degrees=angles, pose_change = tuple(self.robot_pose_change))
+            self.robot_pose_change = np.array([0.0,0.0,0.0])
+            self.previous_distances = distances.copy()
+            self.previous_angles    = angles.copy()
         elif self.previous_distances is not None:
             # If not adequate, use previous
             self.slam.update(self.previous_distances, scan_angles_degrees=self.previous_angles)
 
         # Get current robot position
         x, y, theta = self.slam.getpos()
-        
+
         # Get current map bytes as grayscale
         self.slam.getmap(self.mapbytes)
 
@@ -97,15 +138,14 @@ class Slam(Node):
         pos.y = float(y)
         pos.theta = float(theta)
         self.publisher_position.publish(pos)
-        
+
         # Send the map in a topic
         map_message = Map()
-        self.get_logger().info("Sending map {} to subscribers -- ".format(self.map_index) + str(type(self.mapbytes[0])))
+        # self.get_logger().info("Sending map {} to subscribers -- ".format(self.map_index) + str(type(self.mapbytes[0])))
         map_message.map_data = self.mapbytes
         map_message.index = self.map_index
-        # the size of this map is indeed 500 X 500 = 250 000
         self.publisher_map.publish(map_message)
-        self.map_index += 1 
+        self.map_index += 1
 
 
 
