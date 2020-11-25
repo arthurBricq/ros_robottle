13429c5bfdb6a88bb0b19d7c24bdbb7e23332abb
Arthur Bricq <arthur.bricq@epfl.ch>
2020-11-25 13:08:49 +0100

    ready to deployment

---
+++ src/robottle/launch/launch_controller.launch.py
@@ -0,0 +1,51 @@
+import launch
+import launch.actions
+import launch.substitutions
+import launch_ros.actions
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return launch.LaunchDescription([
+        ### INPUT NODES
+        Node(
+            package="robottle",
+            node_executable="uart_reader",
+            name="uart reader",
+            output="screen",
+            emulate_tty=True
+            ),
+        Node(
+            package="robottle",
+            node_executable="lidar",
+            name="lidar",
+            output="screen",
+            emulate_tty=True
+            ),
+        Node( 
+            package="robottle",
+            node_executable="vision_analyser",
+            name="vision_analyser"
+            ),
+        ### INTERNAL NODES
+        Node( 
+            package="robottle",
+            node_executable="slam",
+            name="slam"
+            ),
+        Node(
+            package="robottle",
+            node_executable="controller1",
+            name="controller1",
+            output="screen",
+            emulate_tty=True
+            ),
+        ### OUTPUT NODES
+        Node(
+            package="robottle",
+            node_executable="uart_sender",
+            name="uart",
+            output="screen",
+            emulate_tty=True
+            ),
+        ])
