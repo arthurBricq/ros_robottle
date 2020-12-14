9576edcd02505d11a1fd5824df459fdefdafd320
Arthur Bricq <arthur.bricq@epfl.ch>
2020-12-14 19:36:15 +0100

    adding ne launch file for bottle test

---
+++ src/robottle/launch/bottle_picking.launch.py
@@ -0,0 +1,31 @@
+import launch
+import launch.actions
+import launch.substitutions
+import launch_ros.actions
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return launch.LaunchDescription([
+        Node(
+            package="robottle",
+            node_executable="vision_analyser",
+            name="vision_analyser",
+            output="screen",
+            emulate_tty=True
+            ),
+        Node(
+            package="robottle",
+            node_executable="uart_reader",
+            name="uart reader",
+            output="screen",
+            emulate_tty=True
+            ),
+        Node(
+            package="robottle",
+            node_executable="uart_sender",
+            name="uart",
+            output="screen",
+            emulate_tty=True
+            ),
+        ])
