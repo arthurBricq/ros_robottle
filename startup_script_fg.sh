eea82f44504c526de0e36a32935cfb0ad74c3fa6
Arthur Bricq <arthur.bricq@epfl.ch>
2021-01-05 14:09:14 +0100

    adding second script

---
+++ startup_script_fg.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# source ROS2
+. install/setup.sh
+
+# launch all the nodes
+# ros2 run robottle teleop & 
+ros2 launch ros_deep_learning detectnet.ros2.launch input:=csi://0 output:=display://0 & ros2 launch robottle bottle_picking.launch.py ; fg
+
+# ros2 launch ros_deep_learning detectnet.ros2.launch input:=csi://0 output:=display://0 &
+# ros2 launch robottle bottle_picking.launch.py && fg
