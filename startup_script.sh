bd287a7b9432b7ffbaa589c4858b6125875eeef3
Arthur Bricq <arthur.bricq@epfl.ch>
2021-01-05 12:47:10 +0100

    updating startup script

--- startup_script.sh
+++ startup_script.sh
@@ -3,7 +3,7 @@
 
 # launch all the nodes
 # ros2 run robottle teleop & 
-ros2 launch ros_deep_learning detectnet.ros2.launch input:=csi://0 output:=display://0 & ros2 launch robottle bottle_picking.launch.py fg
+ros2 launch ros_deep_learning detectnet.ros2.launch input:=csi://0 output:=display://0 & ros2 launch robottle bottle_picking.launch.py ; fg
 
 # ros2 launch ros_deep_learning detectnet.ros2.launch input:=csi://0 output:=display://0 &
 # ros2 launch robottle bottle_picking.launch.py && fg
