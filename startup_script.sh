390fe13366bcc6e024a70f5f7871fe5c0268d575
Arthur Bricq <arthur.bricq@epfl.ch>
2021-01-07 14:18:29 +0100

    updates

--- startup_script.sh
+++ startup_script.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 # source ROS2
-git pull
+cd $ROS_PATH
 source /home/arthur/dev/ros/ros.sh
-colcon build
-
-# . /home/arthur/dev/ros/workspace1/install/setup.sh
+git pull
+colcon build --packages-select robottle
 
 # launch all the nodes
 # ros2 run robottle teleop & 
-ros2 launch ros_deep_learning detectnet.ros2.launch input:=csi://0 output:=display://0 & ros2 launch robottle launch_nocontroller.launch.py & ros2 run robottle controller1 --search; fg
+ros2 launch ros_deep_learning detectnet.ros2.launch input:=csi://0 output:=display://0 & ros2 launch robottle launch_nocontroller.launch.py & ros2 run robottle controller1 --search; 
