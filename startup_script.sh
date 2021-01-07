5722939afb7368c2d2ebf39e8197901a29248d33
Arthur Bricq <arthur.bricq@epfl.ch>
2021-01-07 14:11:24 +0100

    updates

--- startup_script.sh
+++ startup_script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # source ROS2
+git pull
 source /home/arthur/dev/ros/ros.sh
-. /home/arthur/dev/ros/workspace1/install/setup.sh
+colcon build
+
+# . /home/arthur/dev/ros/workspace1/install/setup.sh
 
 # launch all the nodes
 # ros2 run robottle teleop & 
-ros2 launch ros_deep_learning detectnet.ros2.launch input:=csi://0 output:=display://0 & ros2 launch robottle bottle_picking.launch.py ; 
-
-# ros2 launch ros_deep_learning detectnet.ros2.launch input:=csi://0 output:=display://0 &
-# ros2 launch robottle bottle_picking.launch.py && fg
+ros2 launch ros_deep_learning detectnet.ros2.launch input:=csi://0 output:=display://0 & ros2 launch robottle launch_nocontroller.launch.py & ros2 run robottle controller1 --search; fg
